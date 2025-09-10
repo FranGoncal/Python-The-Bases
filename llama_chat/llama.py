58ea259d77743b07b6b4b96c94a1b08ab81ec757
FranGoncal <franciscogoncalvesep@gmail.com>
2025-09-10 20:54:34 +0100

    ChatBot Small Implementation

---
+++ llama_chat/llama.py
@@ -0,0 +1,35 @@
+from groq import Groq
+import os
+
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+
+
+
+while True:
+
+    messages = []
+
+    user_input = input("You: ")
+    if user_input.lower() == "quit":
+        break
+    
+    messages.append({"role": "user", "content": user_input})
+
+    completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=messages,
+        temperature=1,
+        max_completion_tokens=1024,
+        top_p=1,
+        stream=True,
+        stop=None
+    )
+
+    print("Bot: ", end="")
+    for chunk in completion:
+        print(chunk.choices[0].delta.content or "", end="")
+    print()    
